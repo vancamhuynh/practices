4c6688494038d2916cebcc11044a1557399c0c84
Van Huynh <vanhuynh@192-168-1-2.tpgi.com.au>
2018-12-16 15:52:41 +1100

    cannot save student

--- myapp/app.js
+++ myapp/app.js
@@ -2,43 +2,50 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
-const port = 3000
+const port = 3001
 
 app.get('/', (req, res) => res.send('Hello Van!!'))
 
 app.listen(port, () => console.log(`Example app listen in port ${port}!!!`))
 
-// var mongoAtlas = 'mongodb+srv://vanhuynh:<Oldman1908>@cluster0-buffa.mongodb.net/test?retryWrites=true'
-var mongoAtlas = 'mongodb+srv://vanhuynh:<Oldman1908>@cluster0-buffa.mongodb.net/admin'
+var mongoAtlas = 'mongodb+srv://vanhuynh:Oldman1908@cluster0-buffa.mongodb.net/admin'
+// var mongoAtlas = 'mongodb+srv://andyluu:oldman1908@cluster0-buffa.mongodb.net/admin'
+ mongoose.connect(mongoAtlas, function(error) {
+   if(error) throw error;
 
-mongoose.connect(mongoAtlas, { useNewUrlParser: true })
 
-// var db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'MongoDB connect error!!!'))
+   var db = mongoose.connection;
+   db.on('error', console.error.bind(console, 'MongoDB connect error!!!'))
 
-// var Schema = mongoose.Schema;
-//
-// var studentSchema = new Schema({
-//   id: Number,
-//   name: String
-// })
-//
-// var studentModel = mongoose.model('studentModel', studentSchema)
-//
-// var student = new studentModel({
-//   id: 123,
-//   name: "Vicky"
-// })
+   console.log("connect DB successfully!!");
+   var Schema = mongoose.Schema;
+   var studentSchema = new Schema({
+     id: Number,
+     name: String
+   })
 
-// student.save(error => {
-//   if (error) console.error('Cannot save student!!!')
-// });
-//
-// console.log('finish create student, now search')
+   var studentModel = mongoose.model('studentModel', studentSchema)
 
-// studentModel.find({id: 123}, (error, student) => {
-//   if (error) console.error(error)
-//   if (student) console.log(`found: ${student}`)
-// })
+   var student = new studentModel({
+     id: 123,
+     name: "Vicky"
+   })
+
+   console.log("start saving students");
+
+   student.save(function(error, student) {
+     if (error) throw (error)
+     console.log('finish created student, now search')
+     console.log(student);
+
+   //   studentModel.find({id: 123}, (error, student) => {
+   //     if (error) console.error(error)
+   //     if (student) console.log(`found: ${student}`)
+   //   }
+   // });
+
+ })
+})
+
 
 mongoose.connection.close()
